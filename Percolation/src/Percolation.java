edf0336a57ad8b8c701b058762ad106ce080c258
nurmerey <shakhanova.n@gmail.com>
2016-12-04 23:38:02 +1000

    fixed styling and few bugs to pass tests

--- Percolation/src/Percolation.java
+++ Percolation/src/Percolation.java
@@ -3,87 +3,61 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 import java.util.Arrays;
 
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class Percolation {
 private int n;
 private WeightedQuickUnionUF wquf;
 private int[] openSites;
 
-    public static void main(String[] args) {
-        Percolation p = new Percolation(3);
-        System.out.println("hello");
-        StdOut.println(Arrays.toString(p.openSites));
-        StdOut.println(p.isTop(2));
-        StdOut.println(p.isBottom(8));
-        StdOut.println(p.isLeftMost(7));
-        StdOut.println(p.isRightMost(3));
-        p.open(1, 1);
-        p.open(1, 2);
-        p.open(3, 2);
-        p.open(2, 2);
-        p.open(2, 3);
-        StdOut.println(Arrays.toString(p.openSites));
-        
-        StdOut.println("connections ----");
-        StdOut.println(p.wquf.connected(1, 2));
-        StdOut.println(p.wquf.connected(2, 5));
-        StdOut.println(p.wquf.connected(5, 8));
-        StdOut.println(p.wquf.connected(1, 8));
-        StdOut.println(p.wquf.connected(8, 10));
-        StdOut.println("find ----");
-        
-        StdOut.println(p.isFull(3, 2));
-        StdOut.println("--- percolates");
-        StdOut.println(p.percolates());
-    }
     
-//create n-by-n grid, with all sites blocked
+// create n-by-n grid, with all sites blocked
    public Percolation(int n) {  
-       if(n <= 0) throw new IllegalArgumentException("n is less than 0!");
+       if (n <= 0) throw new IllegalArgumentException("n is less than 0!");
        this.n = n;
        this.wquf = new WeightedQuickUnionUF(this.n*this.n+2);
        this.openSites = new int[this.n*this.n+2];
-       this.openSites[0]=1; // virtual top
-       this.openSites[this.n*this.n+1]=1; // virtual bottom
+       this.openSites[0] = 1; // virtual top
+       this.openSites[this.n*this.n+1] = 1; // virtual bottom
    }
    
 // open site (row, col) if it is not open already
-   public void open(int row, int col){
+   public void open(int row, int col) {
        this.validIndex(row);
        this.validIndex(col);
        int num = this.rowColTo1D(row, col);
        this.openSites[num] = 1;  
        
-       //check if top, left, bottom, and right are open
-       if(!this.isTop(num)){//check top
-           if(this.isOpen(row-1, col)){
+       // check if top, left, bottom, and right are open
+       if (!this.isTop(num)) { // check top
+           if (this.isOpen(row-1, col)) {
                int topNum = this.rowColTo1D(row-1, col);
-               if(!this.wquf.connected(num, topNum)){
+               if (!this.wquf.connected(num, topNum)) {
                    this.wquf.union(num, topNum);
                }
            }
        }
-       if(!this.isBottom(num)){//check bottom
-           if(this.isOpen(row+1, col)){
+       if (!this.isBottom(num)) { // check bottom
+           if (this.isOpen(row+1, col)) {
                int bottomNum = this.rowColTo1D(row+1, col);
-               if(!this.wquf.connected(num, bottomNum)){
+               if (!this.wquf.connected(num, bottomNum)) {
                    this.wquf.union(num, bottomNum);
                }
            }
        }
 
-       if(!this.isLeftMost(num)){//check left
-           if(this.isOpen(row, col-1)){
+       if (!this.isLeftMost(num)) { // check left
+           if (this.isOpen(row, col-1)) {
                int leftNum = this.rowColTo1D(row, col-1);
-               if(!this.wquf.connected(num, leftNum)){
+               if (!this.wquf.connected(num, leftNum)) {
                    this.wquf.union(num, leftNum);
                }
            }
        }
-       if(!this.isRightMost(num)){//check right
-           if(this.isOpen(row, col+1)){
+       if (!this.isRightMost(num)) { // check right
+           if (this.isOpen(row, col+1)) {
                int rightNum = this.rowColTo1D(row, col+1);
-               if(!this.wquf.connected(num, rightNum)){
+               if (!this.wquf.connected(num, rightNum)) {
                    this.wquf.union(num, rightNum);
                }
            }
@@ -95,7 +69,7 @@ private int[] openSites;
        this.validIndex(row);
        this.validIndex(col);
        int num = this.rowColTo1D(row, col);
-       if(this.openSites[num]==1){
+       if (this.openSites[num] == 1) {
            return true;
        }
        return false;
@@ -106,65 +80,90 @@ private int[] openSites;
        this.validIndex(row);
        this.validIndex(col);
        int num = this.rowColTo1D(row, col);
-       if(this.wquf.connected(0, num)){
+       System.out.println("find virtual bottom parent "+row+" "+col+" "+this.wquf.connected(num, this.n*this.n+1));
+       System.out.println(this.wquf.find(num));
+       if (this.wquf.connected(0, num) && this.isOpen(row, col)) {
            return true;
        }
+
+       
        return false;
    }
    
 // does the system percolate?
    public boolean percolates() {
-       if(this.wquf.connected(0, this.n*this.n+1)){
+       if (this.wquf.connected(0, this.n*this.n+1)) {
            return true;
        }
        return false;
    }
    
-   private boolean isTop(int x){
+   private boolean isTop(int x) {
        this.validSquareIndex(x);
-       if(x <= this.n+1){
-           this.wquf.union(0, x);//unite with virtual bottom
+       if (x <= this.n) {
+           this.wquf.union(0, x); // unite with virtual bottom
            return true;
        }
        return false;
    }
    
-   private boolean isBottom(int x){
+   private boolean isBottom(int x) {
        this.validSquareIndex(x);
-       if(x >= (this.n*(this.n-1)+1)){
-           this.wquf.union(x, this.n*this.n+1);//unite with virtual bottom
+       if (x >= (this.n*(this.n-1)+1)) {
+           System.out.println("CONNECTING TO Virtual Bottom! "+x);
+           this.wquf.union(x, this.n*this.n+1); // unite with virtual bottom           
            return true;
        }
        return false;
    }
    
-   private boolean isLeftMost(int x){
+   private boolean isLeftMost(int x) {
        this.validSquareIndex(x);
-       if(x==1 || x % this.n ==1){
+       if (x == 1 || x % this.n == 1) {
            return true;
        }
        return false;
    }
    
-   private boolean isRightMost(int x){
+   private boolean isRightMost(int x) {
        this.validSquareIndex(x);
-       if(x % this.n  == 0){
+       if (x % this.n  == 0) {
            return true;
        }
        return false;
    }
    
-   private void validIndex(int index){
+   private void validIndex(int index) {
        if (index <= 0 || index > this.n) throw new IndexOutOfBoundsException("row index i out of bounds");
    }
    
-   private void validSquareIndex(int index){
+   private void validSquareIndex(int index) {
        if (index <= -1 || index > (this.n*this.n+2)) throw new IndexOutOfBoundsException("square index i out of bounds");
    }
    
-   private int rowColTo1D(int row, int col){
+   private int rowColTo1D(int row, int col) {
        int id = this.n*(row-1)+col;
        return id;
+   }
+   
+   public static void main(String[] args) {
+       Percolation p = new Percolation(10);
+       System.out.println("hello");
+       while(!p.percolates()){
+           int row = StdRandom.uniform(1, 10+1);
+           int col = StdRandom.uniform(1, 10+1);
+           if (!p.isOpen(row, col)) {
+               p.open(row, col);
+           }
+       }
+       StdOut.println("find ----"+p.isFull(9, 1));
+       StdOut.println("find ----"+p.isFull(10, 1));
+       StdOut.println("find ----"+p.isFull(10, 2));
+       
+       
+//       StdOut.println(p.isFull(3, 2));
+       StdOut.println("--- percolates");
+       StdOut.println(p.percolates());
    }
    
 }
--- Percolation/src/PercolationStats.java
+++ Percolation/src/PercolationStats.java
@@ -1,7 +1,5 @@
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
-import edu.princeton.cs.algs4.StdIn;
-import edu.princeton.cs.algs4.StdOut;
 
 public class PercolationStats {
     
@@ -9,44 +7,50 @@ public class PercolationStats {
    private int size;
    private int trials;
    
-   public PercolationStats(int n, int trials){
-	  if(n <= 0 || trials <= 0) throw new IllegalArgumentException("n or trials is <= 0");
-	  this.size = n*n;
-	  this.trials = trials;
-	  timesItTookToPerculate = new double[trials];
-	  for(int i=0; i<trials; i++){
-	      Percolation p = new Percolation(n);
-	      double openSiteCount = 0;
-	      while(!p.percolates()){
-	          p.open(StdRandom.uniform(1, n+1), StdRandom.uniform(1, n+1));
-	          openSiteCount++;
-	      }
-	      System.out.println("has finished perculating "+openSiteCount);
-	      timesItTookToPerculate[i] = openSiteCount;
-	  }
-	  System.out.println("double array "+timesItTookToPerculate[0]);
+   public PercolationStats(int n, int trials) {
+      if (n <= 0 || trials <= 0) throw new IllegalArgumentException("n or trials is <= 0");
+      this.size = n*n;
+      this.trials = trials;
+      timesItTookToPerculate = new double[trials];
+      for (int i = 0; i < trials; i++) {
+          Percolation p = new Percolation(n);
+          double openSiteCount = 0;
+          while (!p.percolates()) {
+              int row = StdRandom.uniform(1, n+1);
+              int col = StdRandom.uniform(1, n+1);
+              if (!p.isOpen(row, col)) {
+                  p.open(row, col);
+                  openSiteCount++;
+              }
+          }
+          timesItTookToPerculate[i] = openSiteCount;
+      }
    }
-   public double mean(){
-	   return StdStats.mean(timesItTookToPerculate)/this.size;
+   
+   public double mean() {
+       return StdStats.mean(timesItTookToPerculate)/this.size;
    }
-   public double stddev(){
-	   return StdStats.stddev(timesItTookToPerculate)/this.size;
+   
+   public double stddev() {
+       return StdStats.stddev(timesItTookToPerculate)/this.size;
    }
-   public double confidenceLo(){
-       double lo = this.mean()-((1.96*this.stddev())/this.trials);
-	   return lo;
+   
+   public double confidenceLo() {
+       double lo = this.mean()-((1.96*this.stddev())/Math.sqrt(this.trials));
+       return lo;
    }
-   public double confidenceHi(){
-       double hi = this.mean()+((1.96*this.stddev())/this.trials);
-	   return hi;
+   
+   public double confidenceHi() {
+       double hi = this.mean()+((1.96*this.stddev())/Math.sqrt(this.trials));
+       return hi;
    }
 
-   public static void main(String[] args){
+   public static void main(String[] args) {
        int n = Integer.parseInt(args[0]);
        int t = Integer.parseInt(args[1]);
-	   PercolationStats ps = new PercolationStats(n, t);
-	   System.out.println("mean                    = "+ps.mean());
-	   System.out.println("stddev                  = "+ps.stddev());
-	   System.out.println("95% confidence interval = "+ps.confidenceLo()+", "+ps.confidenceHi());
+       PercolationStats ps = new PercolationStats(n, t);
+       System.out.println("mean                    = "+ps.mean());
+       System.out.println("stddev                  = "+ps.stddev());
+       System.out.println("95% confidence interval = "+ps.confidenceLo()+", "+ps.confidenceHi());
    }
 }
